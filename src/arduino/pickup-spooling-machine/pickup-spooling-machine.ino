802bb081626ad917ba4bd75c6220c3e45adb87ef
Mat Janson Blanchet <mat.jb@jansensan.net>
2017-11-28 12:37:58 -0500

    First functional sketch
    First functional sketch:
    - LED is lit when board turns on;
    - Motor rotates and its speed is controlled by the potentiometer.

---
+++ src/arduino/pickup-spooling-machine/pickup-spooling-machine.ino
@@ -0,0 +1,68 @@
+#include <AccelStepper.h>
+#include <Math.h>
+
+
+// constants
+const float MAX_STEPPER_SPEED = 1000.;
+const float MAX_POT_VALUE = 1023.;
+
+
+// vars
+int potInPin = A0;
+int potValue;
+
+int toggleInPin = 7;
+int toggleValue;
+
+int ledOutPin = 13;
+
+float currentSpeed = 0;
+
+
+// defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+AccelStepper stepper;
+
+
+// arduino methods
+void setup() {
+  // set input pins
+  pinMode(potInPin, INPUT);
+  pinMode(toggleInPin, INPUT_PULLUP);
+
+  // set output pins
+  pinMode(ledOutPin, OUTPUT);
+
+  // init stepper
+  stepper.setMaxSpeed(MAX_STEPPER_SPEED);
+
+  // turn light on
+  digitalWrite(ledOutPin, HIGH);
+}
+
+void loop() {
+  updateMotorSpeed();
+  stepper.runSpeed();
+}
+
+
+// methods definitions
+void updateMotorSpeed() {
+  potValue = analogRead(potInPin);
+
+  // calculate new speed from pot input
+  int newSpeed = floor((MAX_STEPPER_SPEED / MAX_POT_VALUE) * potValue);
+
+  // limit values
+  if (newSpeed > MAX_STEPPER_SPEED) {
+    newSpeed = MAX_STEPPER_SPEED;
+  } else if (newSpeed < 0) {
+    newSpeed = 0;
+  }
+
+  // assign new speed if different
+  if (newSpeed != currentSpeed) {
+    currentSpeed = newSpeed;
+  }
+
+  stepper.setSpeed(currentSpeed);
+}
